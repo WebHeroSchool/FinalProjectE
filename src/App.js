d63477216df1993926f9d8cf737aae9816717276
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-08 13:35:05 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-const element = (<div>
-  Hello World!
-  <a href="">go to </a>
+import React from 'react';
+
+const ItemList = () => (<ul>
+  <li>1</li>
+  <li>2</li>
+</ul>);
+
+const element = (<h1>Hello World!</h1>);
+
+const App = () => (<div>
+  {element}
+  <ItemList />
 </div>);
 
-export {element};
+export default App;
