2cafbd1fe4adc9373ff82ccdcf17ad84d42bfafb
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-08 12:51:35 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import count from './numbers.js';
+import { length } from './numbers.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p style={{
+          color: 'red',
+          fontSize: 40
+        }}>
+          Hello world!
+        </p>
+        <p>
+          {count*length}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/numbers.js
@@ -0,0 +1,5 @@
+const count = 89;
+const length = 11;
+
+export default count;
+export { length };
