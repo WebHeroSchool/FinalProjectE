28db712daf0a94b670f13f784d568996e9421423
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-10 13:49:32 +0400

    Delete src/components directory

--- src/components/Item/Item.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Item = () => (<span>Очень важное дело!</span>);
-const InputItem = () => (<div>InputItem</div>);
-const Footer = () => (<div>Footer</div>)
-
-export { Item, InputItem, Footer };
