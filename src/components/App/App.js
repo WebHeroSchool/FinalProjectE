6d160013e955ffa276575e1accea056e9e0ce331
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-10 13:38:44 +0400

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ItemList from '../ItemList/ItemList';
+import { InputItem, Footer } from '../Item/Item.js';
 
 const App = () => (
   <div>
+    <h1>Todos</h1>
+    <InputItem />
     <ItemList />
+    <Footer />
   </div>);
 
 export default App;
