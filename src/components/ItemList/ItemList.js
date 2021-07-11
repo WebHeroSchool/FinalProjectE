6951d935dd5d32b80b8264ab5a42828a5481da7e
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-11 12:08:30 +0400

    Update ItemList.js

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Item from '../Item/Item.js';
 
 const ItemList = ({ items }) => (<ul>
-  {items.map(item => <li key={items}>
+  {items.map(item => <li key={1}>
     <Item value={item.value} />
   </li>)}
 </ul>);
