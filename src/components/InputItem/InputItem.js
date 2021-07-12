4d5cf9f6c76ab0c5ccf9461770eda690d05fe865
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-12 20:01:55 +0400

    Add files via upload

---
+++ src/components/InputItem/InputItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+
+const InputItem = () => (<div>
+	<TextField id="standard-search" label="Добавить задание" type="search" />
+</div>);
+
+export default InputItem;
