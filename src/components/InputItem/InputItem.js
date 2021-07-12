95da847b3bd07fcc019dc8d5807a9add8d256baf
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-12 19:53:50 +0400

    Delete src directory

--- src/components/InputItem/InputItem.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-
-const InputItem = () => (<div>
-	<TextField id="outlined-search" label="Добавить Задание" type="search" variant="outlined" />
-</div>);
-
-export default InputItem;
