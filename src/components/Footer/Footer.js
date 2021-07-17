00a3d4ecb92c7e83b5750c1989f8561574306a3b
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-17 12:33:52 +0400

    Delete src directory

--- src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Footer.module.css'
-
-const Footer = ({ taskCount }) => (
-	<div className={styles.tasks}> Осталось выполнить: {taskCount} дел</div>
-);
-
-export default Footer;
