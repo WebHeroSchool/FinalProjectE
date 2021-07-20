66d2c4edb15dc3d672650230df80ea5e9f836a5a
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-20 11:05:27 +0400

    Delete src/components directory

--- src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Footer.module.css'
-
-const Footer = ({ taskCount, onClickFooter }) => (
-	<div className={styles.tasks} onClick={onClickFooter}> Осталось выполнить: {taskCount} дел</div>
-);
-
-export default Footer;
