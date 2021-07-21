b5db54dd6d29094b29832c741cd34c7b319d149b
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-21 13:23:25 +0400

    Add files via upload

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import styles from '../App/App.module.css';
+
+const About = () => {
+	// <div className={styles.underWrap}>
+	// 	<div className={styles.wrap}>
+	// 		<h1 className={styles.title}>About</h1>
+	// 	</div>
+	// </div>
+	<div>About</div>
+};
+
+export default About;
---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import styles from '../App/App.module.css';
+
+const Contacts = () => {
+	<div className={styles.underWrap}>
+		<div className={styles.wrap}>
+			<h1 className={styles.title}>Contacts</h1>
+		</div>
+	</div>
+};
+
+export default Contacts;
