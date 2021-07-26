38a48100e6e58f2230fddb0593a14a4c4d0ed91b
Kri0k <68000966+Kri0k@users.noreply.github.com>
2021-07-26 16:42:05 +0400

    Add files via upload

---
+++ src/components/InputItem/InputItem.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import styles from './InputItem.module.css';
+import PropTypes from 'prop-types';
+
+class InputItem extends React.Component {
+	state= {
+		inputValue: ''
+	};
+	onButtonClick= () => {
+	this.setState({
+		inputValue: ''
+	})
+	this.props.onClickAdd(this.state.inputValue);
+};
+	render () {
+		const { onClickAdd } = this.props;
+
+		return (
+			<div className={styles.heightController}>
+				<TextField
+					id="standard-search"
+					label={"Добавить задание"}
+					type="search"
+					value={this.state.inputValue}
+					onChange={event => this.setState({inputValue: event.target.value})}
+				/>
+				<div className={styles.blank}>Hello</div>
+				<Button
+					variant='contained'
+					fullWidth
+					onClick={() => onClickAdd(this.state.inputValue)}
+					onClick={this.onButtonClick}
+				>
+					Добавить
+				</Button>
+			</div>
+		)
+	}
+}
+
+InputItem.propTypes = {
+	value: PropTypes.string.isRequired,
+	onChange: PropTypes.func,
+	onClick: PropTypes.func,
+	onClickAdd: PropTypes.func
+};
+
+export default InputItem;
---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Item from '../Item/Item.js';
+import styles from './ItemList.module.css'
+import PropTypes from 'prop-types';
+
+import List from '@material-ui/core/List';
+
+const ItemList = ({items, onClickDone, isDone, id, onClickDelete}) => (
+  <List className={ styles.itemList}>
+    {items.map(item => (
+      <Item
+        key={item.id}
+        value={item.value}
+        isDone={item.isDone}
+        onClickDone={onClickDone}
+        onClickDelete={onClickDelete}
+        id={item.id}
+      />
+    ))}
+ </List>
+);
+
+ItemList.propTypes = {
+  value: PropTypes.string.isRequired,
+  key: PropTypes.number.isRequired,
+  isDone: PropTypes.bool,
+  onClickDone: PropTypes.func,
+  onClickDelete: PropTypes.func
+}
+
+export default ItemList;
